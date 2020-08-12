bfdd31279ec4b0b5c6b27f9b5df7534c41b86353
Fuad Suyudi <fuad@Fuad-MacBook-Air.local>
2020-08-12 08:34:46 +0700

    change to OOP

--- villagers.php
+++ villagers.php
@@ -1,45 +1,56 @@
 <?php
 
-function FindAverage($person)
-{
-    $born = $kills = $sum_kills = [];
-    $kill = 1;
-    $old = 0;
+class Average {
+    public $person;
 
-    foreach ($person as $key => $val) {
-        $person_born = $val[1] - $val[0];
-        if ($person_born > 1) {
-            $born[] = $person_born;
-        } else {
-            echo -1;
-
-            exit;
-        }
+    function set_person($person) {
+        $this->person = $person;
     }
 
-    for ($i=0; $i <= max($born); $i++) { 
-        $kills[] = $old;
+    function get_average() {
+        $born = $kills = $sum_kills = [];
+        $kill = 1;
+        $old = 0;
 
-        $dum = $kill;
-        $kill += $old;
-        $old = $dum;
+        foreach ($this->person as $key => $val) {
+            $person_born = $val[1] - $val[0];
+            if ($person_born > 1) {
+                $born[] = $person_born;
+            } else {
+                return -1;
 
-        if(in_array($i, $born)){
-            $sum_kills[] = array_sum($kills);
+                exit;
+            }
         }
-    }
 
-    echo array_sum($sum_kills) / count($person);
+        for ($i=0; $i <= max($born); $i++) { 
+            $kills[] = $old;
+
+            $dum = $kill;
+            $kill += $old;
+            $old = $dum;
+
+            if(in_array($i, $born)){
+                $sum_kills[] = array_sum($kills);
+            }
+        }
+
+        return array_sum($sum_kills) / count($this->person);
+    }   
 }
+
+$find_avg = new Average();
 
 // [
 //      [Age, Year], person A
 //      [Age, Year], person B
 //      [Age, Year], person ...  
 // ]
-FindAverage(
-    [
-        [10, 12], 
-        [13, 17]
-    ]
-);
+$person = [
+    [10, 12], 
+    [13, 17]
+];
+
+$find_avg->set_person($person);
+
+echo $find_avg->get_average();
